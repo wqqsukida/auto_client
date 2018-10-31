6d8649f4afba21f0eaec030c1b2ce61d8e86bd35
wqqsukida <wuyifei@1988107@163.com>
2018-10-31 11:40:08 +0800

    20181031更新: 1.添加获取windows端硬盘信息方法

--- src/plugins/disk.py
+++ src/plugins/disk.py
@@ -57,12 +57,76 @@ class Disk(BasePlugin):
         return False
     
     def win(self,cmd_func,test):
-        rep = {}
+        result = {}
         c = wmi.WMI()
-        for drive in c.Win32_LogicalDisk(DriveType=3):
 
-            space = 100.0 * long(drive.FreeSpace) / long(drive.Size)
+
+        for disk in c.Win32_DiskDrive():
+            disk_info = {}
+            disk_info['slot'] = '%s'%disk.SCSIBus
+            disk_info['capacity'] = '%.2f'%(float(disk.Size)/1024/1024/1024)
+            disk_info['model'] = disk.Model
+            disk_info['pd_type'] = disk.InterfaceType
+            result[disk.SCSIBus] = disk_info
+            # space = 100.0 * long(drive.FreeSpace) / long(drive.Size)
             # print "%s has %0.2f%% free" % (drive.Name, space)
 
-
-        return rep
+        return result
+'''
+{'_associated_classes': None,
+ 'methods': {u'Reset': None, u'SetPowerState': None},
+ 'property_map': {},
+ 'properties': {
+                   u'Index': None,
+                   u'TracksPerCylinder': None,
+                   u'PowerManagementSupported': None,
+                   u'MaxMediaSize': None,
+                   u'NumberOfMediaSupported': None,
+                   u'Capabilities': None,
+                   u'SCSIPort': None,
+                   u'MinBlockSize': None,
+                   u'SystemName': None,
+                   u'PowerManagementCapabilities': None,
+                   u'Status': None,
+                   u'PNPDeviceID': None,
+                   u'Description': None,
+                   u'TotalCylinders': None,
+                   u'BytesPerSector': None,
+                   u'ConfigManagerUserConfig': None,
+                   u'ErrorCleared': None,
+                   u'InterfaceType': None,
+                   u'CompressionMethod': None,
+                   u'SCSITargetId': None,
+                   u'Manufacturer': None,
+                   u'SerialNumber': None,
+                   u'Name': None,
+                   u'InstallDate': None,
+                   u'MediaType': '<wmi_property: MediaType>',
+                   u'DefaultBlockSize': None,
+                   u'Signature': None,
+                   u'Size': '<wmi_property: Size>',
+                   u'FirmwareRevision': None,
+                   u'CapabilityDescriptions': None,
+                   u'ConfigManagerErrorCode': None,
+                   u'ErrorMethodology': None,
+                   u'MediaLoaded': None,
+                   u'SectorsPerTrack': None,
+                   u'Model': None,
+                   u'TotalHeads': None,
+                   u'SCSILogicalUnit': None,
+                   u'SCSIBus': None,
+                   u'Caption': '<wmi_property: Caption>',
+                   u'ErrorDescription': None,
+                   u'NeedsCleaning': None,
+                 u'StatusInfo': None,
+                 u'TotalTracks': None,
+                 u'MaxBlockSize': None,
+                 u'TotalSectors': None,
+                 u'LastErrorCode': None,
+                 u'CreationClassName': None,
+                 u'Partitions': None,
+                 u'Availability': None,
+                 u'SystemCreationClassName': None,
+                 u'DeviceID': None
+ }}
+'''
